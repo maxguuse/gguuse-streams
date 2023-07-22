ca212d8021881d917e12ef8d319dee6d41f072df
Maxim Egorov <86559789+maxguuse@users.noreply.github.com>
2023-07-22 11:59:26 +0300

    Update privMsg.go

--- internal/handlers/privMsg.go
+++ internal/handlers/privMsg.go
@@ -3,7 +3,6 @@ package handlers
 import (
 	"log"
 	"strings"
-	"time"
 
 	"github.com/maxguuse/gguuse-streams/configs/repositories"
 	twitch_config "github.com/maxguuse/gguuse-streams/configs/twitch"
